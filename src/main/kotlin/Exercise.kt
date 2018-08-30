b68da21a916bec963f5970e2e7a444741bb7244f
Jakub Blejder <jakub.blejder@rst.com.pl>
2018-08-30 09:41:55 +0200

    invoke string

---
+++ src/main/kotlin/Exercise.kt
@@ -0,0 +1,6 @@
+/*
+    Your goal is to make this compiles:
+    "I am String"("I am the argument")
+
+    Just return the argument
+ */
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,31 @@
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
+
 
 class Test {
 
     @Test
-    fun test() {
-        Assert.assertTrue(true)
+    fun strings() {
+        assertEquals("I am the argument!", "I am the string invoke me!"("I am the argument!"))
+        assertEquals("hello", "I am the string invoke me!"("hello"))
     }
+
+    /*
+    If you feel brave enough uncomment tests below
+     */
+//    @Test
+//    fun ints() {
+//        val r = Random()
+//        repeat(100) {
+//            r.nextInt().also { assertEquals(it, "another one"(it)) }
+//        }
+//    }
+//
+//    @Test
+//    fun longs() {
+//        val r = Random()
+//        repeat(100) {
+//            r.nextLong().also { assertEquals(it, "another one"(it)) }
+//        }
+//    }
 }
