444bb0769bcf4e17bfc164908c934f2137dbd68f
Jakub Blejder <jakub.blejder@rst.com.pl>
2018-08-30 09:30:11 +0200

    extension constructor

---
+++ src/main/kotlin/Exercise.kt
@@ -0,0 +1,8 @@
+/*
+   Kotlin support something like extension methods, so you can add method to existing classes
+
+   Your goal is to write extension constructor.
+
+   Add constructor for kotlin.Int that accepts String and parse this string to Int.
+ */
+
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,39 @@
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
+
 
 class Test {
 
     @Test
-    fun test() {
-        Assert.assertTrue(true)
+    fun `fake constructor for Int`() {
+        val r = Random()
+        repeat(100) {
+            val num = r.nextInt()
+            Assert.assertEquals(num, kotlin.Int(num.toString()))
+        }
     }
+
+
+    /*
+    Uncomment this if you feel brave enough to pass following tests too
+     */
+
+//    @Test
+//    fun `fake constructor for Double`() {
+//        val r = Random()
+//        repeat(100) {
+//            val num = r.nextDouble()
+//            Assert.assertEquals(num, kotlin.Double(num.toString()))
+//        }
+//    }
+
+//    @Test
+//    fun `fake constructor for Long`() {
+//        val r = Random()
+//        repeat(100) {
+//            val num = r.nextLong()
+//            Assert.assertEquals(num, kotlin.Long(num.toString()))
+//        }
+//    }
 }
