3e6b3be891f9633402b00b5e4f8f3843cb5fb677
Jakub Blejder <jakub.blejder@rst.com.pl>
2018-08-30 07:00:47 +0200

    set up tests

---
+++ src/main/kotlin/One.kt
@@ -0,0 +1,1 @@
+fun giveMeOne() = 1
---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,10 @@
+import org.junit.Assert
+import org.junit.Test
+
+class Test {
+
+    @Test
+    fun one() {
+        Assert.assertEquals(1, giveMeOne())
+    }
+}
