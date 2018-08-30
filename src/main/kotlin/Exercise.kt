5a6c14c16b0436363ca231ef632691246fc21b4d
Jakub Blejder <jakub.blejder@rst.com.pl>
2018-08-30 09:53:29 +0200

    three functions

---
+++ src/main/kotlin/Exercise.kt
@@ -0,0 +1,11 @@
+/*
+    The goal is to write three following functions
+
+    guard - the opposite of if
+    until - the opposite of while
+    forceRun - whatever happen inside (exception), just continue
+ */
+
+fun guard() {}
+fun until() {}
+fun forceRun() {}
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,46 @@
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class Test {
 
+    private fun testGuardHelper(boolean: Boolean) {
+        var int = 1
+        guard(boolean) { int = 2 }
+        assertEquals(if (boolean) 1 else 2, int)
+    }
+
+
+    private fun testUntilHelper(boolean: Int) {
+        var int = 1
+        var cond = boolean
+        until({ (cond--) == 0 }) { int++ }
+        assertEquals(boolean + 1, int)
+    }
+
     @Test
-    fun test() {
-        Assert.assertTrue(true)
+    fun testUnless() {
+        val r = java.util.Random()
+        repeat(30) {
+            testGuardHelper(r.nextBoolean())
+        }
+    }
+
+    @Test
+    fun testUntil() {
+        val r = java.util.Random()
+        repeat(30) {
+            testUntilHelper(Math.abs(r.nextInt(10)))
+        }
+    }
+
+    @Test
+    fun testForceRun() {
+        val r = java.util.Random()
+        repeat(30) {
+            forceRun { listOf(1)[r.nextInt()] }
+        }
+        var a = 1
+        forceRun { a = 2 }
+        assertEquals(a, 2)
     }
 }
