13753f46586036268ce0f65ac06737008e1e1e1c
Jakub Blejder <jakub.blejder@rst.com.pl>
2018-08-30 10:10:20 +0200

    concat strings

---
+++ src/main/kotlin/Exercise.kt
@@ -0,0 +1,8 @@
+/*
+    The task is to write function like below but you cannot use 'fun', '{' or '}'
+
+    this function works but does not pass tests.
+
+    WARNING - you should remove this comment to make tests pass
+ */
+fun concatString(s1: String, s2: String) = s1 + s2
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,39 @@
 import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Test
+import java.io.File
+import java.io.IOException
+import java.util.*
 
 class Test {
 
     @Test
-    fun test() {
-        Assert.assertTrue(true)
+    fun cannotUseFun() = try {
+        val property = System.getProperty("user.dir")
+        File("$property/src/main/kotlin/Exercise.kt").useLines {
+            it.forEach {
+                Assert.assertFalse("cannot use fun", "fun" in it)
+                Assert.assertFalse("cannot use {", "{" in it)
+                Assert.assertFalse("cannot use }", "}" in it)
+            }
+        }
+    } catch (e: IOException) {
+        fail("Failed to read the source file.")
+    }
+
+    @Test
+    fun fixedTest() {
+        assertEquals("onetwo", concatString("one", "two"))
+    }
+
+    @Test
+    fun randomTest() {
+        val r = Random()
+        repeat(100) {
+            val a = r.nextInt().toString(2)
+            val b = r.nextInt().toString(2)
+            assertEquals(a + b, concatString(a, b))
+        }
     }
 }
